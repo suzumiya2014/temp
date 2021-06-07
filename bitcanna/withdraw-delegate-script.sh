03d9f6d5c189ce9f56b59042b387b6d0ac70bcd6
suzumiya2014 <782298335@qq.com>
2021-06-08 01:15:20 +0800

    Update withdraw-delegate-script.sh

--- bitcanna/withdraw-delegate-script.sh
+++ bitcanna/withdraw-delegate-script.sh
@@ -1,1 +1,26 @@
+#!/bin/sh
 
+rewards=$(bcnad query distribution commission bcnavaloper19xxs503q3xe6p20tngk4qa8shlp47973udwysk | sed 's/,/\n/g' | grep "amount" | sed 's/:/\n/g' | sed 's/\"//g' | sed '1d' | sed 's/}//g')
+echo $rewards
+minnum=1000000
+numtemp=$(echo "$rewards >$minnum" |bc)
+if [ 1 -gt $numtemp ]
+then
+    bcnad tx distribution withdraw-all-rewards --from suzumiya2014 --chain-id bitcanna-testnet-2 --gas-prices 0.01ubcna --yes
+fi
+
+amount=$(bcnad query bank balances bcna19xxs503q3xe6p20tngk4qa8shlp479739sly2z | sed 's/,/\n/g' | grep "amount" | sed 's/:/\n/g' | sed 's/\"//g' | sed '1d' | sed 's/}//g')
+echo $amount
+
+minnum=1000000
+staynum=100000
+tempdaric=ubcna
+delegatenum=$(expr $amount - $staynum)
+echo $delegatenum
+delegateprice=$delegatenum$tempdaric
+
+if [ ${amount} -gt $minnum ] 
+then
+    bcnad tx staking delegate bcnavaloper19xxs503q3xe6p20tngk4qa8shlp47973udwysk $delegateprice --from suzumiya2014 --chain-id bitcanna-testnet-2 --gas-prices 0.01ubcna --yes
+    break
+fi
